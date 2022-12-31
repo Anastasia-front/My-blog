d4573dd5ed3b465f799865b70b0b52fb4a612e73
Anastasia-front <palitsanastasia3.ap@gmail.com>
2022-12-31 20:24:27 +0200

    final version

--- js/photo.js
+++ js/photo.js
@@ -1,5 +1,5 @@
 // ----------------------LINKS to-------------------------------
-const l = document.querySelector(".sidebar__list");
+const l = document.querySelector(".sidebar__link");
 const l1 = document.getElementById("1");
 const l2 = document.getElementById("2");
 const l3 = document.getElementById("3");
@@ -10,7 +10,7 @@ const c2 = subT[1];
 const c3 = subT[2];
 // console.log(c1, c2, c3);
 // ----------------------COORDINATES by-------------------------------
-const t1 = c1.getBoundingClientRect().top + 100;
+const t1 = c1.getBoundingClientRect().top - 500;
 const t2 = c2.getBoundingClientRect().top;
 const t3 = c3.getBoundingClientRect().top;
 // console.log(t1, t2, t3);
@@ -24,7 +24,10 @@ const observer = new IntersectionObserver(
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         document.querySelectorAll(".sidebar__link").forEach((link) => {
-          link.classList.toggle("active", getId(link) === entry.target.id);
+          link.classList.toggle(
+            "photo-active",
+            getId(link) === entry.target.id
+          );
         });
       }
     });
@@ -35,10 +38,10 @@ const observer = new IntersectionObserver(
 );
 
 document
-  .querySelectorAll(".subtitle")
+  .querySelectorAll(".gallery__subtitle")
   .forEach((subtitle) => observer.observe(subtitle));
 
-document.querySelector(".sidebar__list").addEventListener("click", (event) => {
+document.querySelector(".sidebar__link").addEventListener("click", (event) => {
   if (event.target.classList.contains("")) {
     event.preventDefault();
     const id = getId(event.target);
@@ -53,23 +56,14 @@ document.querySelector(".sidebar__list").addEventListener("click", (event) => {
 // ----------------------NAVIGATION BY LINKS-------------------------------
 
 l1.addEventListener("click", (e) => {
-  c1.scrollIntoView(".subtitle");
+  c1.scrollIntoView(".gallery__subtitle");
   e.preventDefault();
 });
 l2.addEventListener("click", (e) => {
-  c2.scrollIntoView(".subtitle");
+  c2.scrollIntoView(".gallery__subtitle");
   e.preventDefault();
 });
 l3.addEventListener("click", (e) => {
-  c3.scrollIntoView(".subtitle");
+  c3.scrollIntoView(".gallery__subtitle");
   e.preventDefault();
 });
-
-// ----------------------SCROLL PHOTO-------------------------------
-
-// const fixed = document.querySelector(".lock-padding");
-// const photo = document.querySelector(".photo");
-
-// if (fixed.style.display != "none") {
-//   photo.style.top = "50px";
-// }
